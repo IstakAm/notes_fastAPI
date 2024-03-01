da6239ccd2a3eef53ef6ea5ee38a1551378c6512
IstakAm <m.amokhtaari@gmail.com>
2024-03-02 01:10:05 +0330

    dockerized main app and daabase

--- database/connect.py
+++ database/connect.py
@@ -1,14 +1,19 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+c_string = os.getenv('DATABASE_URL')
 username = 'postgres'
 password = 'postgres'
 host = 'localhost'  # or the IP address/hostname of your PostgreSQL server
-port = '5432'  # default PostgreSQL port
+port = '5434'  # default PostgreSQL port
 database = 'firstapi'
 
 # Construct the connection string
-connection_string = f'postgresql://{username}:{password}@{host}:{port}/{database}'
+connection_string = "postgresql://postgres:postgres@notes_db:5432/firstapi"
 print(connection_string)
 
 engine = create_engine(connection_string, echo=True)
